63377a493b9d24537f3af44fdce189415014441c
Mohamed Mahsoub <56096335+Mohmmed-Mahsoub@users.noreply.github.com>
2020-01-18 09:23:17 +0200

    add

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+/*global $, alert, console*/
+
+$(function () {
+
+    'use strict';
+    
+    // trigger nice scroll
+    
+    $("html").niceScroll({
+        
+        cursorcolor: "#fd833d",
+        cursorwidth: "5px",
+        cursorborder: "#fd833d"
+        
+    });
+});
